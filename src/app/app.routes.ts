2f7cf7d891dd492dd8a61b326cf20e2dc72c7faa
miawlfs <35786328+miawlfs@users.noreply.github.com>
2018-07-17 21:07:39 -0500

    pprueva de responsive
    ver en  gogle

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { ServbenComponent } from './components/servben/servben.component';
+import { ProductosComponent } from './components/productos/productos.component';
+
 const APP_ROUTES : Routes = [
   { path: 'home', component: HomeComponent },
 { path: 'servicios', component: ServbenComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'servicios' }
+{ path: 'productos', component: ProductosComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
